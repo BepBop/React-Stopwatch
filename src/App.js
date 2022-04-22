cf04534059001baeab3aa8b7691518078fdbaade
Acer <aa>
2022-04-22 18:38:48 +0530

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import './App.scss';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 export function App() {
-	const [Sec, setSec] = useState(0);
+  const [Sec, setSec] = useState(0);
 
-	const [Hand, setHand] = useState(0);
-	const [Flag, setFlag] = useState(false);
+  const [Hand, setHand] = useState(0);
+  const [Flag, setFlag] = useState(false);
 
-	if (Sec === 60) {
-		setSec(0);
-		setHand(Hand + 1);
-	}
+  if (Sec === 60) {
+    setSec(0);
+    setHand(Hand + 1);
+  }
 
-	useEffect(() => {
-		if (Flag) {
-			const id = setInterval(() => {
-				setSec((sec) => sec + 1);
-			}, 1000);
+  useEffect(() => {
+    if (Flag) {
+      const id = setInterval(() => {
+        setSec((sec) => sec + 1);
+      }, 1000);
 
-			return () => {
-				clearInterval(id);
-			};
-		}
-	}, [Flag]);
+      return () => {
+        clearInterval(id);
+      };
+    }
+  }, [Flag]);
 
-	return (
-		<>
-			<div className="time">{`${Hand} : ${Sec} `}</div>
+  return (
+    <>
+      <div className={"clock"}>{`${Hand} : ${Sec} `}</div>
 
-			<div className="buttons">
-				<button
-					type="button"
-					onClick={() => {
-						setFlag(!Flag);
-					}}
-				>
-					<div>{Flag ? '▶️' : '⏸️'} </div>
-				</button>
-			</div>
-		</>
-	);
+      <button
+        type="button"
+        onClick={() => {
+          setFlag(!Flag);
+        }}
+      >
+        {!Flag ? "▶" : "⏸"}
+      </button>
+
+    </>
+  );
 }
