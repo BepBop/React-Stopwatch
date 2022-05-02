88e2bb94305830fe62a3e64d1ab48a0c7555c3b0
Acer <aa>
2022-05-02 21:35:40 +0530

    fixed CSS bug

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import { Input } from './Input';
 import { Watch } from './Watch';
 
 export function App() {
-	const [InputValue, setInputValue] = useState('');
+
 	const [Arr, setArr] = useState(['']);
 	return (
 		<div className={classes.flex}>
 			<Input
-				Input={InputValue}
-				setInput={setInputValue}
 				fun={(event) => {
-					setArr((prev) => [...prev, event]);
+					setArr((Arr) => [...Arr, event]);
 				}}
 			/>
 			{Arr.map((event) => (
--- src/Input.js
+++ src/Input.js
@@ -1,17 +1,21 @@
+import {useState} from "react";
 import * as classes from "./style.module.css";
 
 export function Input(props) {
+
+	const [inputValue, set_inputValue] = useState('');
+
 	const onSubmit = (event) => {
 		event.preventDefault();
-		props.fun(props.Input);
+		props.fun(inputValue);
 	};
 	return (
 		<form onSubmit={onSubmit}>
 			<input
 				placeholder="Title"
 				className={classes.input}
-				value={props.Input}
-				onChange={(event) => props.setInput(event.target.value)}
+				value={inputValue}
+				onChange={(event) => set_inputValue(event.target.value)}
 			/>
 		</form>
 	);
